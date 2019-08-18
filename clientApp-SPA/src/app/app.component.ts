d47cc2f458647db7e33811af93b212c7e04b8bd0
hasitha <hasithamadusanka90@gmail.com>
2019-08-18 16:09:14 +0530

    bug fix

--- clientApp-SPA/src/app/app.component.ts
+++ clientApp-SPA/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private authService: AuthService){
+
+  }
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if(token) {
+      this.authService.decodedToken = this.authService.jwtHelper.decodeToken(token);
+    }
+  }
 }
--- clientApp-SPA/src/app/nav/nav.component.ts
+++ clientApp-SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -26,8 +26,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
