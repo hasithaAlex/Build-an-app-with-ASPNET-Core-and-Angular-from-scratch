a57601d0409de8bfcb68f4e467077c30a03bb6c4
hasitha <hasithamadusanka90@gmail.com>
2019-08-19 07:22:42 +0530

    changes

--- clientApp-SPA/src/app/app.module.ts
+++ clientApp-SPA/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+import { ErrorInterceptorProvider } from './services/error.interceptor';
+import { appRoutes } from './route.routing';
 
+import { AlertifyService } from './services/alertify.service';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './services/error.interceptor';
-import { AlertifyService } from './services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
@@ -18,13 +23,17 @@ import { AlertifyService } from './services/alertify.service';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
---
+++ clientApp-SPA/src/app/route.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListComponent } from './list/list.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'list', component: ListComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
+
