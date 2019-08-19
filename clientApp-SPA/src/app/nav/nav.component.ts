30aa644e24ac8e34457c3552d866c79e78978e00
hasitha <hasithamadusanka90@gmail.com>
2019-08-19 07:37:03 +0530

    changes

--- clientApp-SPA/src/app/nav/nav.component.ts
+++ clientApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,17 +12,21 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
+    this.authService.login(this.model).subscribe(
+    next => {
       this.alertify.success('logged in successfully!');
     },
     error => {
       this.alertify.error(error);
+    },
+    () => {
+      this.router.navigate(['members']);
     });
   }
 
@@ -31,6 +36,8 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    this.alertify.success('logged out!');
+    this.alertify.message('logged out!');
+    this.router.navigate(['home']);
+
   }
 }
