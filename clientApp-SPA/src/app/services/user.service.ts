d53eef11f86819ab9f0fd99d95e8b417d52e12a3
hasitha <hasithamadusanka90@gmail.com>
2019-08-25 07:00:32 +0530

    changes

--- clientApp-SPA/src/app/services/user.service.ts
+++ clientApp-SPA/src/app/services/user.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/User';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token');
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users');
+    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baseUrl + 'users/' + id);
+    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
   }
 }
