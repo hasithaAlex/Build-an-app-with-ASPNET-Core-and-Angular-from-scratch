406b838ded04bff74066dc13ebf4d1ec3d66b2a2
hasitha <hasithamadusanka90@gmail.com>
2019-08-28 20:23:50 +0530

    bug fix

--- clientApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ clientApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/User';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,8 +11,10 @@ import { User } from 'src/app/models/User';
 })
 export class MemberEditComponent implements OnInit {
 
+  @ViewChild('editForm') editForm: NgForm;
   user: User;
-  constructor(private route: ActivatedRoute) { }
+  
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -18,4 +22,8 @@ export class MemberEditComponent implements OnInit {
     });
   }
 
+  updateUser() {
+    this.alertify.success('Updated Successfully');
+    this.editForm.reset(this.user);
+  }
 }
